89503ba4f59a9dfd8e8eff88d49413ffe819c208
ZoidbergsCrab <121526813+ZoidbergsCrab@users.noreply.github.com>
2025-02-24 18:54:05 -0700

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,5 +1,5 @@
 window.updatesData = {
     latestVersion: "2.1",
     changelog: "Fixed bugs and added new features!",
-    downloadURL: "https://github.com/ZoidbergsCrab/website-updates/archive/refs/tags/v2.1.zip"
+    downloadURL: "https://github.com/ZoidbergsCrab/website-updates/releases/tag/v2.1"
 };
