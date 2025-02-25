1aeaf5deeed8adadca8afda0bf472a439b608076
ZoidbergsCrab <121526813+ZoidbergsCrab@users.noreply.github.com>
2025-02-24 17:51:10 -0700

    Update updates.js

--- updates.js
+++ updates.js
@@ -1,9 +1,5 @@
-(function() {
-    if (window.updatesData) return; // Prevent duplicate loading
-
-    window.updatesData = {
-        latestVersion: "2.1",
-        changelog: "Fixed bugs and added new features!",
-        downloadURL: "https://github.com/ZoidbergsCrab/website-updates/archive/refs/tags/v2.1.zip"
-    };
-})();
+const updatesData = {
+    latestVersion: "2.1",
+    changelog: "Fixed bugs and added new features!",
+    downloadURL: "https://github.com/ZoidbergsCrab/website-updates/archive/refs/tags/v2.1.zip"
+};
