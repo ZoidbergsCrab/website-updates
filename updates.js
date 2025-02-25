ee070c53e09c1f2a6691d10fe6e73167d0e28be3
ZoidbergsCrab <121526813+ZoidbergsCrab@users.noreply.github.com>
2025-02-24 17:42:15 -0700

    Update and rename updates.json to updates.js

---
+++ updates.js
@@ -0,0 +1,5 @@
+const updatesData = {
+    latestVersion: "2.1",
+    changelog: "Fixed bugs and added new features!",
+    downloadURL: "https://github.com/ZoidbergsCrab/website-updates/archive/refs/tags/v2.1.zip"
+};
